2fba1ec174e7afb0062347ada467eb46a693d8c0
Mayukh Sattiraju <msattir@clemson.edu>
2018-12-10 16:52:40 -0500

    Adding Layers conv

--- darknet.py
+++ darknet.py
@@ -31,5 +31,38 @@ def parse_cfg(cfgfile):
      return blocks
 
 
+def create_modules(blocks):
+     net_info = blocks[0]
+     module_list = nn.ModuleList()
+     prev_filters = 3 $#RGB Image
+     output_filters = []
+  
+     for index, x in enumerate(blocks[1:]):
+           module = nn.Sequential()
+
+           if(x["type"] == "convolutional"):
+                 activation = x["activation"]
+                 try:
+                       batch_norm = int(x["batch_normalize"])
+                       bias = False
+                 except:
+                       batch_norm = 0
+                       bias = True
+
+                 filters = int(x["filters"])
+                 padding = int(x["pad"])
+                 kernel_size = int(x["size"])
+                 stride = int(x["stride"])
+
+                 if padding:
+                       pad = (kernel_size -1)//2
+                 else:
+                       pad = 0
+    
+                 #add THE layer
+                 conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+                 module.add_module("conv_{0}".format(index), conv)
+
+
 b=parse_cfg("cfg/yolov3.cfg")
 a=4

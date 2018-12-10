3a8cb8485429dd3cf5ab1a9f4a4c8b6230b9d9e9
Mayukh Sattiraju <msattir@clemson.edu>
2018-12-10 11:33:28 -0500

    Yolov3 pasrse config file

---
+++ darknet.py
@@ -0,0 +1,35 @@
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+
+def parse_cfg(cfgfile):
+     file = open(cfgfile, 'r')
+     lines = file.read().split('\n')
+     lines = [x for x in lines if len(x) > 0]
+     lines = [x for x in lines if x[0] != '#']
+     lines = [x.rstrip().lstrip() for x in lines]
+
+     block = {}
+     blocks = []
+     
+     for line in lines:
+          if line[0] == '[':
+               if len(blocks) != 0:
+                    blocks.append(block)
+                    block = {}
+               block["type"] = line[1:-1].rstrip()
+          else:
+               key,value = line.split("=")
+               block[key.rstrip()] = value.lstrip()
+     blocks.append(block)
+
+     return blocks
+
+
+b=parse_cfg("cfg/yolov3.cfg")
+a=4
